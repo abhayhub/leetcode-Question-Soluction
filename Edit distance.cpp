418fd1baecc1b68a00eef71c45b5736d02dde7a3
abhayhub <63862955+abhayhub@users.noreply.github.com>
2021-03-07 12:40:36 +0530

    Create Edit distance.cpp

---
+++ Edit distance.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minDistance(string A, string B) {
+          int M = A.size(), N = B.size();
+        vector<vector<int>> dp(M + 1, vector<int>(N + 1, INT_MAX));
+        for (int i = 0; i <= M; ++i) {
+            for (int j = 0; j <= N; ++j) {
+                if (i == 0 || j == 0) dp[i][j] = i + j;
+                else if (A[i - 1] == B[j - 1]) dp[i][j] = dp[i - 1][j - 1];
+                else dp[i][j] = 1 + min({ dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1] });
+            }
+        }
+        return dp[M][N];
+        
+    }
+};
